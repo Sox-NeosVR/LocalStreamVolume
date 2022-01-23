caa5489500e32d5831357f7b8c79e6238159c4eb
Sox <66733654+Sox-NeosVR@users.noreply.github.com>
2022-01-23 22:22:29 +0000

    Exist

---
+++ LocalStreamVolume/LocalStreamVolume.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+
+namespace LocalStreamVolume
+{
+    public class LocalStreamVolume : NeosMod
+    {
+        public override string Name => "LocalStreamVolume";
+        public override string Author => "Sox";
+        public override string Version => "1.0.0";
+        public override string Link => "";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.Sox.LocalStreamVolume");
+            harmony.PatchAll();
+        }
+		
+        [HarmonyPatch(typeof(AudioStreamController), "BuildUI")]
+        class LocalStreamVolumePatch
+        {
+            static void Postfix(AudioStreamController __instance, SyncRef<AudioOutput> ____audioOutput)
+            {
+             ValueUserOverride<float> valueoverride =__instance.Slot.AttachComponent<ValueUserOverride<float>>();
+                valueoverride.CreateOverrideOnWrite.Value = true;
+                valueoverride.Target.Target = ____audioOutput.Target.Volume;
+                valueoverride.Default.Value = 1f;
+            }
+        }
+    }
+}
